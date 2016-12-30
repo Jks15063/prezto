3128ac7a5401f69599755bb5c329b90fc497bd73
Jacob Sellers <jks15063@gmail.com>
2016-12-29 17:53:40 -0700

    Fix clipboard
    Thanks @quicknir <3

--- modules/clipboard/init.zsh
+++ modules/clipboard/init.zsh
@@ -1,6 +1,21 @@
-function cutbuffer() {
+#
+# Integrates the zsh clipboard with the system clipboard.
+#
+# Authors:
+#   Nir Friedman <quicknir@gmail.com>
+#
+
+function cutbuffer {
   zle .$WIDGET
-  echo $CUTBUFFER | xclip -selection clipboard
+  if [[ "$OSTYPE" == darwin* ]]; then
+    echo $CUTBUFFER | pbcopy
+  elif [[ "$OSTYPE" == cygwin* ]]; then
+    echo $CUTBUFFER | tee > /dev/clipboard
+  elif (( $+commands[xclip] )); then
+    echo $CUTBUFFER | xclip -selection clipboard
+  elif (( $+commands[xsel] )); then
+    echo $CUTBUFFER | xsel --clipboard --input
+  fi
 }
 
 zle_cut_widgets=(
@@ -16,14 +31,20 @@ zle_cut_widgets=(
   vi-yank-eol
 )
 
-for widget in $zle_cut_widgets
-do
+for widget in $zle_cut_widgets; do
   zle -N $widget cutbuffer
 done
 
-
-function putbuffer() {
-  zle copy-region-as-kill "$(xclip -o -selection clipboard)"
+function putbuffer {
+  if [[ "$OSTYPE" == darwin* ]]; then
+    zle copy-region-as-kill "$(pbcopy)"
+  elif [[ "$OSTYPE" == cygwin* ]]; then
+    zle copy-region-as-kill "$(cat /dev/clipboard)"
+  elif (( $+commands[xclip] )); then
+    zle copy-region-as-kill "$(xclip -o -selection clipboard)"
+  elif (( $+commands[xsel] )); then
+    zle copy-region-as-kill "$(xsel --clipboard --output)"
+  fi
   zle .$WIDGET
 }
 
@@ -32,7 +53,6 @@ zle_put_widgets=(
   vi-put-before
 )
 
-for widget in $zle_put_widgets
-do
+for widget in $zle_put_widgets; do
   zle -N $widget putbuffer
 done
